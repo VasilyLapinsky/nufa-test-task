d003670a431320972635b65e7999be7932d34121
VasilyLapinsky <vasia2001lapa3251@gmail.com>
2023-07-20 20:27:23 +0000

    upd run

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ from diffusers.utils import load_image
 
 
 @click.command()
-@click.option('--content_image', type=str, required=True)
-@click.option('--style_image', type=str, required=True)
-@click.option('--result_image', type=str, default="result.jpg")
+@click.option('--content_image_path', type=str, required=True)
+@click.option('--style_image_path', type=str, required=True)
+@click.option('--result_image_path', type=str, default="result.jpg")
 def main(content_image_path, style_image_path, result_image_path):
     torch.manual_seed(42)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -26,20 +26,29 @@ def main(content_image_path, style_image_path, result_image_path):
     content_image = load_image(content_image_path)
     style_image = load_image(style_image_path)
 
-    images_texts = [content_image, style_image]
-    weights = [0.5, 0.5]
-    out = prior.interpolate(images_texts, weights)
+    images_texts = ["4k photo, high resolution, high detail, fine detail", content_image, style_image]
+    weights = [0.1, 0.6, 0.3]
+    out = prior.interpolate(
+        images_texts,
+        weights,
+        num_inference_steps=25,
+        guidance_scale=4.
+    )
 
     result_image = decoder(
         image=content_image,
         image_embeds=out.image_embeds,
         negative_image_embeds=out.negative_image_embeds,
-        height=768,
-        width=768,
+        height=512,
+        width=512,
         num_inference_steps=100,
-        guidance_scale=15,
-        strength=0.5
+        guidance_scale=10,
+        strength=.5
     ).images[0]
 
     result_image.save(result_image_path)
 
+
+if __name__ == '__main__':
+    with torch.inference_mode():
+        main()
